e50028e637578bf215d4d2c3fb847eb0f1144c86
Stephen Fairbanks <biggste2000@hotmail.com>
2016-12-02 13:26:28 +0000

    First vanilla commit

---
+++ templates/header.php
@@ -0,0 +1,12 @@
+<header class="banner no-side-padding col-md-2">
+  <div class="container-fluid">
+    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
+    <nav class="nav-primary">
+      <?php
+      if (has_nav_menu('primary_navigation')) :
+        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
+      endif;
+      ?>
+    </nav>
+  </div>
+</header>
