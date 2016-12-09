5c9d2a5da0acb8bf8e85a77175c3b703798127e2
Stephen Fairbanks <biggste2000@hotmail.com>
2016-12-09 15:48:41 +0000

    Starting to pull together responsive styling

--- templates/content-single.php
+++ templates/content-single.php
@@ -8,18 +8,18 @@
               <a href="<?php the_field('buy_url'); ?>" class="btn-buy" target="_blank" title="Buy this item.">Buy</a>            
             <?php endif; ?>
             <!-- / End sale button -->
-          </h1>
-
-          
+          </h1>        
 
           <?php if('artblog' == get_post_type($wp_query->post->ID)) { ?>
             <div class="post-meta">
-              <time><i class="fa fa-calendar"></i> <?php the_date(); ?></time>
-              <div class="post-cat">
-                <?php if(has_category()) { ?>
-                  <i class="fa fa-folder-open-o"></i> <?php the_category(', '); ?>
-                <?php } ?>
-              </div>
+              <time class="post-meta-child"><i class="fa fa-calendar"></i> <?php the_date(); ?></time>
+              <?php if(has_category()) { ?>
+                <div class="post-cat post-meta-child">
+                  <?php if(has_category()) { ?>
+                    <i class="fa fa-folder-open-o"></i> <?php the_category(', '); ?>
+                  <?php } ?>
+                </div>
+              <?php } ?>
             </div>
           <?php } else { ?>
 
@@ -36,7 +36,7 @@
 
     <div class="row">
 
-      <div class="<?php if('artblog' == get_post_type($wp_query->post->ID)) { ?>col-lg-5<?php } else { ?>col-lg-4<?php } ?> single-post-text">
+      <div class="<?php if('artblog' == get_post_type($wp_query->post->ID)) { ?>col-md-12 col-lg-7 col-xl-7 col-xxl-5<?php } else { ?>col-lg-6 col-xl-6 col-xxl-5<?php } ?> single-post-text">
 
         <div class="entry-content">
           <?php the_content(); ?>
@@ -80,7 +80,7 @@
         // check if the post has a Post Thumbnail assigned to it.
         if ( has_post_thumbnail() ) { ?>
 
-        <div class="col-lg-5">
+        <div class="<?php if('artblog' == get_post_type($wp_query->post->ID)) { ?>col-md-8 col-lg-5 artblog-thumb-col<?php } else { ?>col-lg-6 col-xl-6 col-xxl-5 gallery-col<?php } ?>">
 
           
          
@@ -96,14 +96,14 @@
 
                 <!-- SLIDE 2 -->
                 <div class="image-slide">
-                  <a href="<?php kdmfi_get_featured_image_src( 'featured-image-2'); ?>" target="_blank" title="See full image.">
+                  <a href="<?php echo kdmfi_get_featured_image_src( 'featured-image-2'); ?>" target="_blank" title="See full image.">
                     <?php kdmfi_the_featured_image( 'featured-image-2', 'single-page-img' );  ?>
                   </a>
                 </div>
 
                 <!-- SLIDE 3 -->
                  <div class="image-slide">
-                  <a href="<?php kdmfi_get_featured_image_src( 'featured-image-3'); ?>" target="_blank" title="See full image.">
+                  <a href="<?php echo kdmfi_get_featured_image_src( 'featured-image-3'); ?>" target="_blank" title="See full image.">
                     <?php kdmfi_the_featured_image( 'featured-image-3', 'single-page-img' );  ?>
                   </a>
                 </div>
